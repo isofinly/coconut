9223ce8ad3d0339cd925883ddfa3796416603115
Kirill Parkhomenko <kxrxh.contact@gmail.com>
2023-09-29 23:36:22 +0300

    port fixes!

--- scrapper-api/app.py
+++ scrapper-api/app.py
@@ -3,12 +3,14 @@ from typing import Any, Dict, List
 
 from scrapper import extract_keywords_multilang, extract_metadata, extract_paragraphs, get_domain, get_site_pages, load_models
 
-app = Flask(__name__)
+app = Flask("scrapper-api")
 
 # Load language models when the app starts
 load_models()
 
-
+@app.get('/')
+def index() -> str:
+    return 'Hello, World!'
 @app.route('/extract_keywords', methods=['POST'])
 def extract_keywords() -> Dict[str, Any]:
     """
@@ -22,22 +24,21 @@ def extract_keywords() -> Dict[str, Any]:
         A dictionary containing the detected language, model name, 
         and keywords.
     """
-    if request.method == 'POST':
-        data: Dict[str, Any] = request.json
-        text: str = data.get('text', '')
-        detected_language: str
-        model_name: str
-        keywords: List[str]
+    data: Dict[str, Any] = request.json
+    text: str = data.get('text', '')
+    detected_language: str
+    model_name: str
+    keywords: List[str]
 
-        detected_language, model_name, keywords = extract_keywords_multilang(
-            text)
+    detected_language, model_name, keywords = extract_keywords_multilang(
+        text)
 
-        result: Dict[str, Any] = {
-            'detected_language': detected_language,
-            'model_name': model_name,
-            'keywords': keywords
-        }
-        return jsonify(result)
+    result: Dict[str, Any] = {
+        'detected_language': detected_language,
+        'model_name': model_name,
+        'keywords': keywords
+    }
+    return jsonify(result)
 
 
 @app.route('/get_domain', methods=['POST'])
@@ -51,11 +52,10 @@ def get_domain_handler() -> str:
     Returns:
         str: The domain extracted from the base URL.
     """
-    if request.method == 'POST':
-        data = request.json
-        base_url: str = data.get('url', '')
-        domain: str = get_domain(base_url)
-        return jsonify({'domain': domain})
+    data = request.json
+    base_url: str = data.get('url', '')
+    domain: str = get_domain(base_url)
+    return jsonify({'domain': domain})
 
 
 @app.route('/get_site_pages', methods=['POST'])
@@ -70,11 +70,10 @@ def get_site_pages_handler() -> jsonify:
     Returns:
         jsonify: A JSON response containing the list of page URLs.
     """
-    if request.method == 'POST':
-        data: dict = request.json
-        site_url: str = data.get('url', '')
-        page_urls: List[str] = list(get_site_pages(site_url))
-        return jsonify({'page_urls': page_urls})
+    data: dict = request.json
+    site_url: str = data.get('url', '')
+    page_urls: List[str] = list(get_site_pages(site_url))
+    return jsonify({'page_urls': page_urls})
 
 
 @app.route('/extract_paragraphs', methods=['POST'])
@@ -88,11 +87,10 @@ def extract_paragraphs_handler() -> Response:
     Returns:
         Response: A JSON response containing the extracted paragraphs.
     """
-    if request.method == 'POST':
-        data: Dict[str, Any] = request.json
-        url: str = data.get('url', '')
-        paragraphs: List[str] = extract_paragraphs(url)
-        return jsonify({'paragraphs': paragraphs})
+    data: Dict[str, Any] = request.json
+    url: str = data.get('url', '')
+    paragraphs: List[str] = extract_paragraphs(url)
+    return jsonify({'paragraphs': paragraphs})
 
 
 @app.route('/extract_metadata', methods=['POST'])
@@ -106,12 +104,11 @@ def extract_metadata_handler() -> Dict[str, Any]:
     Returns:
         Dict[str, Any]: The extracted metadata.
     """
-    if request.method == 'POST':
-        data: Dict[str, Any] = request.json
-        url: str = data.get('url', '')
-        metadata: Dict[str, Any] = extract_metadata(url)
-        return jsonify(metadata)
+    data: Dict[str, Any] = request.json
+    url: str = data.get('url', '')
+    metadata: Dict[str, Any] = extract_metadata(url)
+    return jsonify(metadata)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(port=5001, debug=True, host='0.0.0.0')
