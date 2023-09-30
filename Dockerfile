2f8b36acc1e53f12e2dc38fb9cc28db185657128
isofinly <isofinly@icloud.com>
2023-09-30 23:34:00 +0300

    minor changes

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,12 @@ WORKDIR /app
 
 COPY package*.json ./
 
+RUN npx playwright install
+
 RUN npm install
 
 COPY . .
 
 EXPOSE 3050
 
-CMD [ "node", "app.js" ]
+CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
+"use strict";
 const express = require("express");
 const { chromium } = require("playwright");
-const puppeteer = require("puppeteer-extra");
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-
 const app = express();
 const port = 3050;
 
@@ -15,127 +13,111 @@ const userAgentStrings = [
 
 app.use(express.json());
 
-puppeteer.use(StealthPlugin());
-
-app.post("/extract-data", async (req, res) => {
-  const { urls } = req.body;
-
-  if (!urls || !Array.isArray(urls)) {
-    return res
-      .status(400)
-      .json({ error: "Missing or invalid URLs in the request body" });
-  }
-
-  const results = [];
-
-  try {
-    const browser = await chromium.launch({
-      headless: false,
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-blink-features=AutomationControlled",
-      ],
-      slowMo: 100,
-    });
-    const context = await browser.newContext({
-      userAgent:
-        userAgentStrings[Math.floor(Math.random() * userAgentStrings.length)],
-      permissions: ["clipboard-read", "clipboard-write"],
-      viewport: {
-        width: 1388,
-        height: 768,
-      },
-    });
+app.post("/extract_data", async (req, res) => {
+    const { urls } = req.body;
   
-    for (const url of urls) {
-      const page = await context.newPage();
-      
-      const response = await page.goto(url, {
-        waitUntil: 'domcontentloaded', // Wait for DOMContentLoaded event
-      }).catch(() => {
-        return null;
-      });
-      
-      // Wait for an additional 1.5 seconds (1500 milliseconds) to ensure JavaScript has loaded
-      await page.waitForTimeout(1500).catch
-        (() => {
-          return null;
-        });
-      
-  
-      if (response && response.status() === 200) {
-        const title = await page.title();
-        const description = await page
-          .$eval('meta[name="description"]', (meta) => meta.content)
-          .catch(() => {
-            return "error";
-          });
-  
-        let keywords;
-        try {
-          keywords = await page.$eval(
-            'meta[name="keywords"]',
-            (meta) => meta.content
-          );
-        } catch (error) {
-          keywords = "error";
-        }
-  
-        const pTags = await page
-          .$$eval("p", (pElements) => pElements.map((p) => p.textContent))
-          .catch(() => {
-            return ["error"];
-          });
-        const aTags = await page
-          .$$eval("a", (aElements) => aElements.map((a) => a.href))
-          .catch(() => {
-            return ["error"];
-          });
-  
-        // Extract OG meta data
-        const ogTags = await page.$$eval(
-          'meta[property^="og:"]',
-          (metaElements) => {
-            const ogData = {};
-            metaElements.forEach((meta) => {
-              const property = meta.getAttribute("property");
-              const content = meta.getAttribute("content");
-              if (property && content) {
-                ogData[property] = content;
-              }
-            });
-            return ogData;
-          }
-        );
-  
-        const result = {
-          title,
-          description,
-          keywords,
-          //   pTags,
-          //   aTags,
-          ogTags,
-        };
-        // console.log(result);
-        results.push(result);
-      }
-  
-      // Close the page after processing
-      await page.close();
+    if (!urls || !Array.isArray(urls)) {
+      return res
+        .status(400)
+        .json({ error: "Missing or invalid URLs in the request body" });
     }
   
-    await browser.close();
+    const results = [];
   
-    res.json(results);
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ error: "An error occurred while processing the request" });
-  }
+    try {
+      const browser = await chromium.launch({
+        args: [
+          "--no-sandbox",
+          "--disable-setuid-sandbox",
+          "--disable-blink-features=AutomationControlled",
+        ],
+        headless: true,
+        ignoreHTTPSErrors: true,
+      });
   
-});
+      const context = await browser.newContext({
+        userAgent:
+          userAgentStrings[Math.floor(Math.random() * userAgentStrings.length)],
+        viewport: {
+          width: 1388,
+          height: 768,
+        },
+      });
+  
+      const pagePromises = urls.map(async (url) => {
+        const page = await context.newPage();
+        const response = await page.goto(url, {
+          waitUntil: "domcontentloaded", 
+        }).catch(() => null);
+  
+        await page.waitForTimeout(1500).catch(() => null);
+
+        if (response && response.status() < 500 && response.status() !== 404) {
+
+          const title = await page.title().catch(() => "None");
+
+          const description = await page
+            .$eval('meta[name="description"]', (meta) => meta.content)
+            .catch(() => "None");
+  
+          let keywords;
+          
+          try {
+            keywords = await page.$eval('meta[name="keywords"]', (meta) => meta.content);
+          } catch (error) {
+            keywords = "None";
+          }
+  
+          const pTags = await page
+            .$$eval("p", (pElements) => pElements.map((p) => p.textContent))
+            .catch(() => ["None"]);
+  
+          const aTags = await page
+            .$$eval("a", (aElements) => aElements.map((a) => a.href))
+            .catch(() => ["None"]);
+  
+          const ogTags = await page.$$eval(
+            'meta[property^="og:"]',
+            (metaElements) => {
+              const ogData = {};
+              metaElements.forEach((meta) => {
+                const property = meta.getAttribute("property");
+                const content = meta.getAttribute("content");
+                if (property && content) {
+                  ogData[property] = content;
+                }
+              });
+              return ogData;
+            }
+          );
+  
+          const result = {
+            title,
+            description,
+            keywords,
+            ogTags,
+            // pTags
+            // aTags
+          };
+          results.push(result);
+        }
+        await page.close();
+      });
+
+      // Wait for all promises to resolve
+      await Promise.all(pagePromises);
+  
+      await browser.close();
+  
+      res.json(results);
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ error: "An error occurred while processing the request" });
+    }
+  });
+  
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
