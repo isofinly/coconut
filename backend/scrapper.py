5309859cb1498e5f12a3dc15a3232bb31dd6c747
Kirill Parkhomenko <kxrxh.contact@gmail.com>
2023-09-30 18:01:04 +0300

    few changes with scrapper and trends

--- backend/scrapper.py
+++ backend/scrapper.py
@@ -5,22 +5,32 @@ from langdetect import detect
 from urllib.parse import urlparse, urljoin
 from typing import Dict, List, Optional, Tuple, Set
 import concurrent.futures
+import fake_useragent
 
-import nltk
-from nltk.corpus import wordnet
-import numpy as np
-import Levenshtein
-from nltk.corpus.reader.wordnet import Synset
 
 # Disable insecure request warning
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-FILE_EXTENSIONS = ['.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.zip', '.rar', '.jpg', '.jpeg', '.png', '.gif', '.rar']
+FILE_EXTENSIONS = ['.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt',
+                   '.pptx', '.zip', '.rar', '.jpg', '.jpeg', '.png', '.gif', '.rar']
 
 
 MODELS: Dict[str, spacy.Language] = {}
+
+
+def fetch_url_with_headers(url: str):
+    user_agent = fake_useragent.UserAgent()
+    headers = {
+        'User-Agent': user_agent.random,
+        # Задайте корректный referer, если это применимо
+        'Referer': 'https://www.google.com/'
+        # Другие заголовки, если необходимо
+    }
+    response = requests.get(url, headers=headers, verify=False)
+    return response
+
 
 def load_models() -> None:
     """
@@ -35,6 +45,7 @@ def load_models() -> None:
     MODELS["fr"] = spacy.load("fr_core_news_sm")
     MODELS["de"] = spacy.load("de_core_news_sm")
 
+
 def extract_keywords_multilang(text: str) -> Tuple[str, str, List[str]]:
     """
     Extract keywords from multilingual text.
@@ -48,7 +59,8 @@ def extract_keywords_multilang(text: str) -> Tuple[str, str, List[str]]:
     """
     # Cache detected language
     detected_language = detect(text)
-    nlp = MODELS.get(detected_language, MODELS["ru"])  # Default to Russian model
+    # Default to Russian model
+    nlp = MODELS.get(detected_language, MODELS["ru"])
     doc = nlp(text)
     keywords = [token.text for token in doc if token.pos_ in ("NOUN", "ADJ")]
 
@@ -95,9 +107,9 @@ def get_site_pages(url: str) -> Set[str]:
     Returns:
         Set[str]: A set of internal page URLs.
     """
-    response = requests.get(url, verify=False)
+    response = fetch_url_with_headers(url)
     if response.status_code != 200:
-        return set()
+        return set([response.status_code])
 
     soup = BeautifulSoup(response.text, 'html.parser')
     links = soup.find_all('a', href=True)
@@ -114,6 +126,7 @@ def get_site_pages(url: str) -> Set[str]:
         page_urls = set(executor.map(process_link, links))
 
     return page_urls
+
 
 def clean_text(text: str) -> str:
     """
@@ -141,7 +154,7 @@ def extract_paragraphs(url: str) -> List[str]:
     """
     page_paragraphs = []
 
-    response = requests.get(url, verify=False)
+    response = fetch_url_with_headers(url)
     if response.status_code != 200:
         return page_paragraphs
 
@@ -154,7 +167,8 @@ def extract_paragraphs(url: str) -> List[str]:
 
     # Use ThreadPoolExecutor to process paragraphs concurrently
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        paragraphs_lists = list(executor.map(process_paragraph, soup.find_all('p')))
+        paragraphs_lists = list(executor.map(
+            process_paragraph, soup.find_all('p')))
 
     # Flatten the list of lists into a single list of paragraphs
     page_paragraphs = [p for sublist in paragraphs_lists for p in sublist]
@@ -172,28 +186,25 @@ def extract_metadata(url: str) -> Dict[str, Optional[str]]:
     Returns:
         Dict[str, Optional[str]]: A dictionary containing the extracted metadata.
     """
-    response = requests.get(url, verify=False)
+    response = fetch_url_with_headers(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Extract metadata information
+    # Extract metadata information
     metadata = {
         'title': soup.title.string.strip() if soup.title else None,
         'description': soup.find('meta', attrs={'name': 'description'})['content'].strip() if soup.find('meta', attrs={'name': 'description'}) else None,
         'keywords': soup.find('meta', attrs={'name': 'keywords'})['content'].strip() if soup.find('meta', attrs={'name': 'keywords'}) else None,
-        # Add more metadata fields as needed
-        }
-    
+    }
+
     if metadata['description'] and not metadata['keywords']:
-        metadata['keywords'] = extract_keywords_multilang(metadata['description'])[2]
+        metadata['keywords'] = extract_keywords_multilang(
+            metadata['description'])[2]
     return metadata
 
 
-
-
-# if __name__ == "__main__":
-#     # Example usage
-#     site_url = "https://stackoverflow.com/"
-#     metadata = extract_metadata(site_url)
-#     print("Title:", metadata.get('title'))
-#     print("Description:", metadata.get('description'))
-#     print("Keywords:", metadata.get('keywords'))
+if __name__ == "__main__":
+    site_url = "https://stackoverflow.com/"
+    metadata = extract_metadata(site_url)
+    print("Title:", metadata.get('title'))
+    print("Description:", metadata.get('description'))
+    print("Keywords:", metadata.get('keywords'))
